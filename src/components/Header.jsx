24f149a2364fdf7a370049ac573bae3c860e98ab
jundolor <jun.dolor@gmail.com>
2025-09-20 23:26:43 +0800

    NavLink with styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logoImg from "../assets/images/mami-nickz-logo.png"
 
 export default function Header(){
+
+
     return(
       <header>
         <Link className='site-logo' to="/"><img src={logoImg} alt="Mami Nickz Logo" /></Link>
         <nav>
-          <Link to="/host">Host</Link>
-          <Link to="/about">About</Link>
-          <Link to="/snacks">Snacks</Link>
+          <NavLink to="/host" className={({isActive}) => isActive? "active-link": null}>Host</NavLink>
+          <NavLink to="/about" className={({isActive}) => isActive? "active-link": null}>About</NavLink>
+          <NavLink to="/snacks" className={({isActive}) => isActive? "active-link": null}>Snacks</NavLink>
         </nav>
       </header>
     )
