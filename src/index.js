ee6cd9d0d24f322c3c1d97929eed3e37f447ffe7
jundolor <jun.dolor@gmail.com>
2025-09-20 21:26:10 +0800

    init version of host subcategory

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Snacks from "./pages/Snacks/Snacks"
 import SnackDetail from "./pages/Snacks/SnackDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
 import Layout from './components/Layout';
+import HostLayout from './components/HostLayout';
+
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -17,12 +22,17 @@ function App(){
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          
-        <Route index element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/snacks' element={<Snacks />} />
-        <Route path='/snacks/:id' element={<SnackDetail />} />
+          <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/snacks' element={<Snacks />} />
+          <Route path='/snacks/:id' element={<SnackDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='income' element={<Income />} />
+            <Route path='reviews' element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
