8e8530eb056ccb34ffce8d5d55fa9ae1e70e21a9
jundolor <jun.dolor@gmail.com>
2025-09-25 11:52:21 +0800

    snacks navigation rendering and formatting

--- src/pages/Snacks/Snacks.jsx
+++ src/pages/Snacks/Snacks.jsx
@@ -1,8 +1,12 @@
+//import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Snacks(){
+    const [snackParams, setSnackParams] = useSearchParams()
     const [snacks, setSnacks] = React.useState([])
+
+    const snackFilter = snackParams.get("type")
     React.useEffect(() => {
         fetch("api/snacks")
         .then(res => res.json())
@@ -13,7 +17,10 @@ export default function Snacks(){
     }
     , [])
 
-    const snackElements = snacks.map(snack => (
+    const displayedSnacks = snackFilter? snacks.filter(snack => snack.type === snackFilter) : snacks
+    console.log(displayedSnacks)
+
+    const snackElements = displayedSnacks.map(snack => (
         <div key={snack.id} className="snack-tile">
             <Link to={`/snacks/${snack.id}`}>
                 <img src={snack.imageUrl} alt="snack delight" />
@@ -27,9 +34,56 @@ export default function Snacks(){
         </div>
     ))
 
+    function handleFilterChange(key, value) {
+        console.log(key, value)
+        setSnackParams(prevParams => {
+            if(value === null) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+            return prevParams
+        })
+    }
+    /*
+    <Link to="?type=street-food" className="snack-type street-food">Street Food</Link>
+                <Link to="?type=fruit" className="snack-type fruit">Fruits</Link>
+                <Link to="?type=snack-meal" className="snack-type snack-meal">Snack Meal</Link>
+                <Link to="." className="snack-type clear-filters">Clear</Link>
+    */
     return (
         <div className="snack-list-container">
             <h1>Check out our tasty treats</h1>
+            <div className="snack-list-filter-buttons">
+                <button 
+                    className={`snack-type street-food ${snackFilter === "street-food" ? "selected" : ""}`} 
+                    onClick={() => handleFilterChange("type", "street-food")}
+                >
+                    Street Food
+                </button>
+                <button 
+                    className={`snack-type fruit ${snackFilter === "fruit" ? "selected" : ""}`} 
+                    onClick={() => handleFilterChange("type", "fruit")}
+                >
+                    Fruits
+                </button>
+                <button 
+                    className={`snack-type snack-meal ${snackFilter === "snack-meal" ? "selected" : ""}`} 
+                    onClick={() => handleFilterChange("type", "snack-meal")}
+                >
+                    Snack Meal
+                </button>
+                {
+                    snackFilter && 
+                    <button 
+                        className="snack-type clear-filters" 
+                        onClick={() => handleFilterChange("type", null)}
+                    >
+                        Clear
+                    </button>
+                }
+                
+            </div>
             <div className="snack-list">
                 {snackElements}
             </div>
